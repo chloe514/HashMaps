b90242123cfecb5022af094d74068b15306f5fac
chloe514 <chloevans22@gmail.com>
2024-08-13 14:23:05 -0400

    hashmaps

--- src/Nicknames.java
+++ src/Nicknames.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class Nicknames {
-}
+import java.util.HashMap;
+
+
+
+    public class Nicknames {
+        public static void main(String[] args) {
+            // Create a new HashMap to store names and nicknames
+            HashMap<String, String> nicknames = new HashMap<>();
+
+            // Add names and their corresponding nicknames to the HashMap
+            nicknames.put("matthew", "matt");
+            nicknames.put("michael", "mix");
+            nicknames.put("arthur", "artie");
+
+            // Retrieve Matthew's nickname from the HashMap
+            String matthewsNickname = nicknames.get("matthew");
+
+            // Print Matthew's nickname
+            System.out.println("Matthew's nickname is: " + matthewsNickname);
+        }
+    }
+
--- src/Program.java
+++ src/Program.java
@@ -1,2 +1,47 @@
-package PACKAGE_NAME;public class Program {
+import java.util.HashMap;
+
+public class Program {
+    // Method to print all the keys in the given hashmap
+    public static void printKeys(HashMap<String, String> hashmap) {
+        for (String key : hashmap.keySet()) {
+            System.out.println(key);
+        }
+    }
+
+    // Method to print the keys in the hashmap which contain the given text
+    public static void printKeysWhere(HashMap<String, String> hashmap, String text) {
+        for (String key : hashmap.keySet()) {
+            if (key.contains(text)) {
+                System.out.println(key);
+            }
+        }
+    }
+
+    // Method to print the values in the hashmap whose keys contain the given text
+    public static void printValuesOfKeysWhere(HashMap<String, String> hashmap, String text) {
+        for (String key : hashmap.keySet()) {
+            if (key.contains(text)) {
+                System.out.println(hashmap.get(key));
+            }
+        }
+    }
+
+    // Main method to test the Program class
+    public static void main(String[] args) {
+        HashMap<String, String> hashmap = new HashMap<>();
+        hashmap.put("e.g.", "for example");
+        hashmap.put("etc.", "and so on");
+        hashmap.put("i.e.", "more precisely");
+        hashmap.put("lol", "laugh out loud");
+
+        System.out.println("All keys:");
+        printKeys(hashmap);
+
+        System.out.println("\nKeys containing 'e':");
+        printKeysWhere(hashmap, "e");
+
+        System.out.println("\nValues of keys containing 'e':");
+        printValuesOfKeysWhere(hashmap, "e");
+    }
 }
+
